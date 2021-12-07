9c8eaa8ede121521d02db56256db00e97ea04681
ThayronSF <thayronsf@unifei.edu.br>
2021-12-06 21:15:33 -0300

    second commit

--- controllers/pessoasControllers.js
+++ controllers/pessoasControllers.js
@@ -2,7 +2,7 @@ const firebase = require("firebase");
 const config = require("../config"); 
 
 const app = firebase.initializeApp(config.firebaseConfig);
-const db = firebase.firestore(); 
+const db = firebase.firestore(app); 
 
 const addPessoa = async (req, res) => {
     try {
@@ -14,6 +14,72 @@ const addPessoa = async (req, res) => {
     }
 }
 
+const getPessoas = async(req, res)=>{
+    try{
+        const pessoas= await db.collection("pessoas").get();
+        const dataResp = [];
+        
+        if(pessoas.empty){
+            res.status(404).send("Nenhum dado encontrado");
+            
+        }else{
+            pessoas.forEach( (doc)=>{
+                dataResp.push({
+                    id: doc.id,
+                    nome: doc.data().nome,
+                    idade: doc.data().idade,
+                    cpf: doc.data().cpf,
+                })
+            })//Fim do forEach
+            res.send(dataResp);
+            console.log(pessoas);
+        }
+
+    }catch(error){
+        res.status(404).send(error.message)
+    }
+}
+
+const getPessoa = async(req, res)=>{
+    try{
+        const id = req.params.id;
+        const pessoa= await db.collection("pessoas").doc(id).get();
+        if(!pessoa.exists){
+            res.status(404).send("Nenhum registro encontrado com este codigo" + id)
+        }else{
+            res.send(pessoa.data());
+            }
+    }catch(error){
+        res.status(404).send(error.message)
+    }
+}
+
+const setPessoa = async(req, res)=>{
+    try{
+        const id = req.params.id;
+        const data = req.body;
+        const pessoa= await db.collection("pessoas").doc(id);
+        await pessoa.uptdate(data);
+        req.status(200).send(data);
+    }catch(error){
+        res.status(400).send(error.message)
+    }
+}
+
+const delPessoa = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const pessoa = await db.collection("pessoas").doc(id).delete();
+        res.status(200).send("Registro excluído");
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
 module.exports = {
     addPessoa,
+    getPessoas,
+    getPessoa,
+    setPessoa,
+    delPessoa,
 }
--- routes/pessoasRotas.js
+++ routes/pessoasRotas.js
@@ -2,11 +2,19 @@ const express = require("express");
 
 const {
     addPessoa,
+    getPessoas,
+    getPessoa,
+    setPessoa,
+    delPessoa,
 } = require("../controllers/pessoasControllers");
 
 const router = express.Router();
 
 router.post("/pessoa", addPessoa); 
+router.get("/pessoas", getPessoas); 
+router.get("/pessoa/:id", getPessoa);
+router.put("/pessoa:id", setPessoa);
+router.delete("/pessoa/:id", delPessoa);
 
 module.exports = {
     routes: router,
